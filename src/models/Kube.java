a8a682fdadde9dfe78d01b6991a79119f3de9fca
Haris-v <harveraros@gmail.com>
2022-05-05 07:46:50 +0300

    Added Kube class

---
+++ src/models/Kube.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Haris
+ */
+public class Kube {
+    private int pleyra;
+
+    public int getPleyra() {
+        return pleyra;
+    }
+
+    public void setPleyra(int pleyra) {
+        this.pleyra = pleyra;
+    }
+    
+    
+}
